179085c83bf0874d56ef18af5b397b3a601a5f8f
Yevhenii Riabchych <evgeny.ryabchich@gmail.com>
2017-11-15 17:10:10 +0200

    Добавил новый интерфейс

---
+++ Telegraph/ITelegramDataService.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace Telegraph
+{
+    public interface ITelegramDataService
+    {
+        BindingList<Telegram> LoadTelegrams();
+
+        bool AddTelegram(Telegram tlg);
+
+        bool EditTelegram(int id, Telegram tlg);
+
+        bool RemoveTelegram(object tlg);
+    }
+}
